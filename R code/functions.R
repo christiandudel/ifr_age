176bf03ae77c827438e700724f6f43d8694e5836
Christian Dudel <dudel@demogr.mpg.de>
2020-10-29 17:36:49 +0100

    Cleaned code

--- R code/functions.R
+++ R code/functions.R
@@ -90,3 +90,67 @@
     return(matching)
     
   }
+  
+  
+### ungroup_lx ######################################################
+  
+  # Ungroup l_x
+  ungroup_lx <- function(lx){
+    
+    # Ages (hard-coded five-year age groups)
+    ages <- c(0,1,seq(5,100,5))
+    
+    # Smoothing spline
+    smooth_lx <- smooth.spline(x=ages,y=lx)
+    
+    # New ages: single years
+    new_age <- c(seq(0,1,1),seq(2,5,1),seq(6,100,1))
+    
+    # Predict from spline
+    predict_lx <- predict(smooth_lx,new_age,len=new_age)
+    
+    # Return
+    return(predict_lx$y)
+    
+  }
+  
+  
+### Calculate life expectancy at age x ##############################
+  
+  lexp_age_specific <- function(lx,ax,mx){  
+    
+    # d_x: Deaths aged x
+    dx <- c(-diff(lx), lx[length(lx)])  
+    
+    # L_x: Person years
+    Lx1 <- lx[-1]+ax[1:(length(ax)-1)]*dx[1:(length(dx)-1)]  
+    Lx2 <- if(mx[length(mx)] == 0) 0 else dx[length(dx)]/mx[length(mx)]  
+    Lx <- c(Lx1, Lx2)  
+    
+    # Cumulative person years  
+    Tx <- rev(cumsum(rev(Lx)))  
+    
+    # Remaining life expectancy at age x
+    ex <- Tx/lx  
+    
+    # If dividion by zero
+    ex[is.nan(ex)] <- 0  
+    
+    # Return result
+    return(ex)    
+    
+  }  
+  
+  
+### Function for weighted row means #################################
+  
+  rowMeansW <- function(data,weight) {
+    
+    # Calculate contributions
+    tmp <- apply(data,1,function(x) x*weights)
+    
+    # Sum
+    colSums(tmp)
+    
+  }
+  
